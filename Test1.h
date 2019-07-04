396fd06cf379811670da65c6fe25f8493812dfc4
gurramvinay <vinaykarthikgurram@gmail.com>
2019-07-04 21:10:24 +0530

    Add files via upload
    Added Test c++ files

---
+++ Test1.h
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <list>
+using namespace std;
+int main () {
+    int T;
+    cin>>T;
+    int n;
+    for (int i=0;i<T;i++)
+    {
+        cin>>n;
+        int arr;
+        int x=n;
+        int temp=n;
+        int a,b,temp1;
+        int test=0;
+        int len=0;
+        while (x!=0)
+        {
+            len++;
+            x=x/10;
+        }
+        if (len%2==0)
+        arr=(len/2)+1;
+        else
+        arr=(len/2);
+        int count[arr];
+        for(int j=0;j<arr;j++)
+        {
+          temp1=temp%100;
+          a=temp1%10;
+          b=temp/10;
+          count[j]=a+b;
+          temp=temp/100;
+        }
+        for (int k=0;k<arr-1;k++)
+        {
+            if(count[k]==count[k+1])
+            {
+                test++;
+                continue;
+            }
+            else{
+                break;
+                cout<<"unlucky";
+            }
+        }
+        if(test==(arr-1))
+        cout<<"lucky";
+    
+    }
+    
+    return 0;
+}
---
+++ Test2.cpp
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+int convert(int);
+int main(void)
+{
+    int a,b,o;
+    int xa,xb;
+scanf("%d",&a);
+printf("\n");
+scanf("%d",&b);
+printf("\n");
+scanf("%d",&o);
+if(a==0)
+xa=0;
+else
+xa=convert(a);
+if(b==0)
+xb=0;
+else
+xb=convert(b);
+if (o=0)
+{
+    printf("%d",xa+xb);
+}
+if (o=1)
+printf("%d",xa*xb);
+    return 0;
+}
+
+int convert (int x)
+{
+    int len=0;
+    int temp=0;
+    int xtemp=0;
+    int pro=1;
+    int sum=0;
+    int y=x;
+    int z=x;
+    int i;
+    while (y!=0)
+    {
+        y=y/10;
+        len++;
+    }
+    for(i=0;i<len;i++)
+    {
+        temp=z%10;
+        
+        if(temp==1)
+        xtemp=9;
+        if(temp==2)
+        xtemp=8;
+        if(temp==3)
+        xtemp=7;
+        if(temp==4)
+        xtemp=6;
+        if(temp==5)
+        xtemp=5;
+        if(temp==6)
+        xtemp=4;
+        if(temp==7)
+        xtemp=3;
+        if(temp==8)
+        xtemp=2;
+        if(temp==9)
+        xtemp=1;
+        
+        z=z/10;
+        if (i=0){
+            sum=xtemp*pro;
+            pro=pro*10;
+        }
+        else
+        {
+            sum=sum+(pro*xtemp);
+            pro=pro*10;
+        }
+    }
+    return (sum);
+}
